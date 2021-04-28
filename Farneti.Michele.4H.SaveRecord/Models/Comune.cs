e4c42c7c2de3d8d0bfe724de0b381b223d6115d5
Farne23 <65703708+Farne23@users.noreply.github.com>
2021-04-28 11:44:40 +0200

    Novita

---
+++ Farneti.Michele.4H.SaveRecord/Models/Comune.cs
@@ -0,0 +1,97 @@
+using System.IO.Enumeration;
+using System.IO;
+using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using Farneti.Michele._4H.SaveRecord.Models;
+
+namespace Farneti.Michele._4H.SaveRecord.Models
+{
+    public class Comune
+    {
+        public int ID { get;  set; }
+        public string NomeComune { get;  set; }
+        public string CodiceCastale { get;  set; }
+
+        public Comune() { }
+
+        public Comune(string s, int id)
+        {
+            string[] splitted = s.Split(',');
+            ID = id;
+            NomeComune = splitted[1];
+            CodiceCastale = splitted[0];
+        }
+    }
+
+    public class Comuni : List<Comune>
+    {
+        public string NomeFile{get;}
+        public Comuni(){}
+
+        public Comuni(string filename)
+        {
+            NomeFile=filename;
+
+            using(FileStream fin = new FileStream(filename, FileMode.Open))
+            {
+                StreamReader reader= new StreamReader(fin);
+
+                string riga;
+                int id = 1;
+
+                while(!reader.EndOfStream)
+                {
+                    riga = reader.ReadLine();
+                    Comune c = new Comune(riga,id);
+                    id++;
+
+                    this.Add(c);                 
+                }
+            }      
+        }
+
+        public void Save()
+        {
+            string fn= NomeFile.Split(".")[0] +".bin";
+            Save(fn);
+        }
+
+        public void Save(string fileName)
+        {
+            FileStream fout = new FileStream(fileName,FileMode.Create);
+            BinaryWriter writer = new BinaryWriter(fout);
+
+            foreach(Comune comune in this)
+            {
+                writer.Write(comune.ID);
+                writer.Write(comune.CodiceCastale);
+                writer.Write(comune.NomeComune);
+            }
+            writer.Flush();
+            writer.Close();
+        }
+
+        public void Load()
+        {
+            string fn= NomeFile.Split(".")[0] +".bin";
+            Load(fn);
+        }
+
+        public void Load(string fileName)
+        {
+            FileStream fin = new FileStream(fileName,FileMode.Open);
+            BinaryReader reader = new BinaryReader(fin);
+
+            Comune c = new Comune();   
+            c.ID = reader.ReadInt32();
+            c.CodiceCastale =reader.ReadString() ;
+            c.NomeComune =reader.ReadString() ;
+        
+            Add(c);
+
+            //MANCA IL CICLO
+        }
+
+    }
+}
---
+++ Farneti.Michele.4H.SaveRecord/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Farneti.Michele._4H.SaveRecord.Models;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farneti.Michele._4H.SaveRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Registro dei comuni di Michele Farneti");
+
+            //Leggere un file csv di comuni e trasformarlo in una lista
+            //Scrivere  la list comuni in un fil binario
+            //RIleggere il file binario una list
+
+            Comuni C= new Comuni("Comuni.csv");   
+            Console.WriteLine($"Ho letto {C.Count} righe da file csv"); 
+
+            C.Save(); 
+
+            Console.WriteLine($"Ho letto {C.Count} righe da file binario"); 
+
+            C.Load();   
+
+        }
+        
+    } 
+}
